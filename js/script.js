82037027c6dc067192913e19b9fea2d5450d5300
Angel Carballeira <angelcarballeira@gmail.com>
2022-11-28 21:15:46 -0300

    Solo bienvenida

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
 window.onload = function () {
   swal({ text: 'Bienvenido a nuestra página', button: 'Ingresar' });
-
-  $('#onload').fadeOut();
-
-  // $('body').removeClass('hidden');
-  let hiddenClass = document.getElementsByClassName('hidden');
-  hiddenClass.classList.remove('hidden');
 };
